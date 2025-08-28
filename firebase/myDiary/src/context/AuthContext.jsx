455bbb0e5baea2f0be6f736ef3e8ce2c952d180f
nugurejeil <nugurejeil1@gmail.com>
2025-08-28 15:53:04 +0900

    0828 수업내용 업로드 (파이어베이스)

--- firebase/myDiary/src/context/AuthContext.jsx
+++ firebase/myDiary/src/context/AuthContext.jsx
@@ -6,6 +6,9 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'login':
             return { ...state, user: action.payload }
+
+        case 'logout':
+            return { ...state, user: null }
         default:
             return state;
     }
