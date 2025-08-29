9d39ad67e689aa948dcca43c8e0e3afc0fc78e91
nugurejeil <nugurejeil1@gmail.com>
2025-08-29 14:29:28 +0900

    0829 중간 업로드

--- firebase/myDiary/src/pages/home/Home.jsx
+++ firebase/myDiary/src/pages/home/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import styles from './Home.module.css'
+import DiaryForm from './DiaryForm'
+import DiaryList from './DiaryList'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 export default function Home() {
+
+    const { user } = useAuthContext();
+
     return (
-        <div>Home</div>
+        <div className="container">
+            <main className={styles["diary-main"]}>
+                <h2 className={styles.heart}>2023.02.27의 비밀일기</h2>
+                <DiaryForm uid={user.uid} />
+            </main>
+            <section>
+                <h2 className="a11y-hidden">일기 목록</h2>
+                <ul>
+                    <DiaryList />
+                </ul>
+            </section>
+        </div>
     )
 }
